488b9fa418e9d6e0c7500de4984e2b5a5c11dc2c
AlexRugara <alexrugara1@gmail.com>
2023-03-16 18:19:30 +0300

    Lets output using function Puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+   * we using puts( ) function to display output
+*/
+int main(void)
+{
+	char str[70] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
