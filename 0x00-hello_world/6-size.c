b51abb02a911145c27ef67c14bf04d83095d0f80
AlexRugara <alexrugara1@gmail.com>
2023-03-16 18:54:44 +0300

    Size of data types in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char : %zu  byte(s)\n", sizeof(char));
+	printf("size of an Int : %zu byte(s) \n", sizeof(int));
+	printf("size of a long int : %zu byte(s)\n", sizeof(long int));
+	return (0);
+
+}
