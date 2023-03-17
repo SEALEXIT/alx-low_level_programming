09cd5cce184cb23628183f73a21683588d7953ee
AlexRugara <alexrugara1@gmail.com>
2023-03-17 17:02:00 +0300

    dont print Q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+
+		if (lc != 'e' && 'q')
+			putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
