e0686f468623289792767982157c4f7e06face93
AlexRugara <alexrugara1@gmail.com>
2023-03-17 18:13:30 +0300

    Print my Hexadecimal numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int hex;
+
+	for (num = 0; num <= 10; num++)
+	{
+
+		putchar((num % 10) + '0');
+	}
+	for (hex = 'a'; hex <= 'f'; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+	return (0);
+}
