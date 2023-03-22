bf8ed85deea851b2badef013fc117fd929209f35
AlexRugara <alexrugara1@gmail.com>
2023-03-22 13:10:10 +0300

    corrections

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,14 @@
 
 /**
   * add - adds two integers
-  * @a - first number
-  * @b - second number
+  * @a: - first number
+  * @b: - second number
   * Return: returns the sum of integers
   */
-int add(int a , int b)
+int add(int a, int b)
 {
 	int sum = a + b;
+
 	return (sum);
 }
 
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,14 @@
 int print_last_digit(int n)
 {
 
-	int last = n % 10;
+	int last;
 
+	last = n % 10;
+	if (last < 0)
+	{
+		last *= -1;
+	}
 	_putchar(last + '0');
-	return (0);
+	return (last);
 }
 
