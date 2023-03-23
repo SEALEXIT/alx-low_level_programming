b77b57e8641680c8fad6a0cee7e0d3a45dd66759
AlexRugara <alexrugara1@gmail.com>
2023-03-23 20:58:25 +0300

    fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,14 +8,17 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
-		else if (i % 5 == 0)
-			printf("Buzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
 			printf("%d", i);
-		printf(" ");
+		if (i != 100)
+			printf(" ");
 	}
+	
+	
 	printf("\n");
 	return (0);
 }
