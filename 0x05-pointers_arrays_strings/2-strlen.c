8f37053826ada69d208e7cd4769122d436fb02d6
AlexRugara <alexrugara1@gmail.com>
2023-03-27 12:21:35 +0300

    Length of my String

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strlen - Types the length of a string
+  * @s: The memory adress of the string
+  * Return: Length of the string
+  */
+
+int _strlen(char *s)
+{
+	int length;
+	length = strlen(s);
+
+	return (length);
+}
