f9a7de3edf8fd266d4292778ed90557dab5b2f4b
AlexRugara <alexrugara1@gmail.com>
2023-03-27 12:34:21 +0300

    Puts prints string with a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _puts - Prints string followed by new line
+  * @str: the memory location of the string
+  * Return: No return Value
+  */
+
+void _puts(char *str)
+{
+	puts(str);
+}
