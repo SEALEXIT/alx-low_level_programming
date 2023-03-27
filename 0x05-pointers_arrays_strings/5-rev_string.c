12ab8837475534e7d5040dbd3985aee0ab79267a
AlexRugara <alexrugara1@gmail.com>
2023-03-27 18:06:26 +0300

    Reverese string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,9 +8,17 @@
 
 void rev_string(char *s)
 {
-	if (*s != '\0')
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
 	{
-		rev_string(s + 1);
-		_putchar(*s);
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
