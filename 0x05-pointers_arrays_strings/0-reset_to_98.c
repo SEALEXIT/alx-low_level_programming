f1a80d2b6adb8a2b2f143b9dda66c2c0b70d2f40
AlexRugara <alexrugara1@gmail.com>
2023-03-27 11:26:44 +0300

    deference to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - resets value of given adress to 98
+  * @n: incoming Memory adress
+  * Return: No value Returned
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
