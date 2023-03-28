3be62a8301a3652c041a01232bd7f64d677b3f27
AlexRugara <alexrugara1@gmail.com>
2023-03-28 10:43:11 +0300

    string Copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strcpy - Coppies string pointed by src
+  * @dest: Thes array
+  * @src: The String
+  *Return: Returns the coppied version
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	return (strcpy(dest, src));
+}
+
