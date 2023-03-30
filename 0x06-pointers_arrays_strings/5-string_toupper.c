c55c667cc5308cf3b7c35593fe3a32e06cb90ba8
AlexRugara <alexrugara1@gmail.com>
2023-03-30 08:25:03 +0300

    convert string to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,11 @@ char *string_toupper(char *st)
 {
 	int i;
 
-
-	for (i = 0; i < sizeof(st); i++)
+	for (i = 0; i != '\0'; i++)
 	{
-		return (strupr(st[i]));
+		if (st[i] >= 'a' && st[i] <= 'z')
+		{
+			st[i] += 32;
+		}
 	}
-
 }
