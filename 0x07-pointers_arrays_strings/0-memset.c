9672f8e38ac7ea45ba847b3f7ec988565f273830
AlexRugara <alexrugara1@gmail.com>
2023-04-03 14:41:18 +0300

    Replace value with constant character..

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _memset - fills memory with constant byte 0x01
+  * @s: The array
+  * @b: the constant byte
+  * @n: number of constants in array
+  * Return: new array
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s + 1, b, n));
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+
