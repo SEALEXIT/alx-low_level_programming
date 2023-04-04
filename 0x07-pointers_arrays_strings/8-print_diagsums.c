4ad32d85cad34cf918d3fda586a04a1c27dfe2c8
AlexRugara <alexrugara1@gmail.com>
2023-04-04 14:09:04 +0300

    diagonals add

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,30 +9,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, res, k, sum, counter;
+	int i, j, k, d1, d2;
 
-	counter = size;
 	k = 0;
-
+	d1 = 0;
+	d2 = 0;
 	for (i = 0; i < size; i++)
-	{
-		for (j = i; j < i + 1; j++)
+		for (j = 0; j < size; j++)
 		{
-			res += a[i][j];
+			(i == j) ? d1 += a[k] : 0;
+			(i + j == size - 1) ? d2 += a[k] : 0;
+			k++;
 		}
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		for (j = counter; j < counter - 1; j++)
-		{
-			sum += a[i][j];
-		}
-	}
-
-	_putchar(res + '0');
-	_putchar(',');
-	_putchar(' ');
-	_putchar(sum + '0');
-	_putchar('\n');
+	printf("%i, %i\n", d1, d2);
 }
