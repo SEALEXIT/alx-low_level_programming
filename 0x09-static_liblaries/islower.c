deb2e76315b1f997ea4cfb6830e6977ac6e56481
AlexRugara <alexrugara1@gmail.com>
2023-04-07 11:12:26 +0300

    creating my static Liblary

---
+++ 0x09-static_liblaries/islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * _islower - checks if character is lowercase
+  * @c: the character
+  * Return: 1 > if lower 0 if not
+  */
+int _islower(int c)
+{
+	return (c);
+}
---
+++ 0x09-static_liblaries/memset.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+char *_memset(char *s1, char b, unsigned int n)
+{
+	return (s1);
+}
