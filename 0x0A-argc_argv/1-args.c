ec3904422ba1a05e64f22c72864c2bbc80e988c5
AlexRugara <alexrugara1@gmail.com>
2023-04-08 14:15:08 +0300

    Print number of arguements passed into the programm argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - prints number of arguements passed in a program
+  * @argc: The number of arguements passed
+  * @argv: Pointer to the string of arguements passed
+  * Return: Zero if successful
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
