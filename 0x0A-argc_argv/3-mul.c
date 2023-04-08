422b9cef04314edd959bb9c591db5fabf2154946
AlexRugara <alexrugara1@gmail.com>
2023-04-08 15:34:28 +0300

    program that takes two arguements from cmd and multiplies and returns an answer

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+  * main - accepts arguements and multiplies them
+  * @argc: number of arguements passed
+  * @argv: Pointer to arguements in string passed
+  * Return: Zero is success
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, ans;
+
+	ans = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+
+	for (i = 1; i < argc; i++)
+	{
+		ans *= atoi(argv[i]);
+	}
+	printf("%d\n", ans);
+	}
+
+	return (0);
+}
+
