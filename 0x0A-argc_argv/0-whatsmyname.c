705cd2d085ab16c83c91abb9eb431e797f1bb8fe
AlexRugara <alexrugara1@gmail.com>
2023-04-08 14:10:05 +0300

    Printing the first argument present --program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 
 /**
   * main - Start of the program
-  * @argc - number of arguements passed
+  * @argc: number of arguements passed
   * @argv: Pointer to string of vector passed to program
   * Return: Zero if program success
   */
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
 	int i;
 
-	for ( i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 
 	printf("%s\n", argv[i]);
