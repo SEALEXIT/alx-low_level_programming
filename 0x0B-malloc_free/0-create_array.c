ef82290e6eee90d250fd9d1f0d17e2a28b86cb6a
AlexRugara <alexrugara1@gmail.com>
2023-04-12 11:06:12 +0300

    allocating memory and assigning memory with constant

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * create_array - pointer function to allocate memory
+  * @size: size of the memory size to be allocated
+  * @c: character of the array
+  * Return: pointer to adress of the memory allocated
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (char*)malloc(size * sizeof(char));
+
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		*(ptr + i) = c;
+	}
+
+	return (ptr);
+
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
