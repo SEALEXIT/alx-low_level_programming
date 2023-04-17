34a6d60a9f838716239cca299f2255109f108ce2
AlexRugara <alexrugara1@gmail.com>
2023-04-17 12:05:57 +0300

    type defination of struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
