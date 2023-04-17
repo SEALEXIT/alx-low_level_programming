2984971e47ae639d00438ca594de18d7d2630503
AlexRugara <alexrugara1@gmail.com>
2023-04-17 14:24:23 +0300

    second commit , definition of struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,16 +9,20 @@
   * @owner: Owner of the dog
   *
   * Description: Longer Description
-  * dog_t - typedef of dog
   */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
+/**
+  * dog_t - type def of dog
+  */
+
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
