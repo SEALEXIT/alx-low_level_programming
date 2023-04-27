dfd27b8d8d8aef86d37c7fca661513e440c354e8
AlexRugara <alexrugara1@gmail.com>
2023-04-27 16:52:45 +0300

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * free_list - free memory list
+  * @head: The pointer 
+  * Return: Nothing
+  */
+
+void free_list(list_t *head)
+{
+	list_t *point;
+
+	while(head != NULL)
+	{
+		point = head->next;
+		free((*head).str);
+		free(head);
+		head = point;
+	}
+}
