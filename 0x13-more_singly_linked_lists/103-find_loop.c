af115659a01582bc9133748734dfdd1ad4a8002b
AlexRugara <alexrugara1@gmail.com>
2023-05-17 14:32:39 +0300

    Advanced tasks

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+  * find_listintt_loop - finds loop in listint_t
+  * @head: pointer to the first node
+  *
+  * Return: Something
+  */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+	return (fast);
+}
